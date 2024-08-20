042d3d0b528e0ebeb26a202c4839471132eca142
Aditya Oberai <adityaoberai1@gmail.com>
2024-08-20 11:51:11 +0000

    Update resume function

--- functions/go-resume/main.go
+++ functions/go-resume/main.go
@@ -1,18 +1,24 @@
 package handler
 
 import (
-	"openruntimes/handler/services"
+	"embed"
 
 	"github.com/open-runtimes/types-for-go/v4/openruntimes"
 )
 
+//go:embed static/*
+var embedReader embed.FS
+
 func Main(Context openruntimes.Context) openruntimes.Response {
 	if Context.Req.Method == "GET" {
-		resumeHtml := services.GetStaticFile(Context, "resume.html")
 
-		return Context.Res.Text(resumeHtml, Context.Res.WithStatusCode(200), Context.Res.WithHeaders(map[string]string{
-			"Content-Type": "text/html; charset=utf-8",
-		}))
+		resumeHtml, _ := embedReader.ReadFile(("static/resume.html"))
+
+		return Context.Res.Text(string(resumeHtml),
+			Context.Res.WithStatusCode(200),
+			Context.Res.WithHeaders(map[string]string{
+				"Content-Type": "text/html",
+			}))
 	}
-	return Context.Res.Text("Bad request", Context.Res.WithStatusCode(400))
+	return Context.Res.Text("Bad request", Context.Res.WithStatusCode(404))
 }
